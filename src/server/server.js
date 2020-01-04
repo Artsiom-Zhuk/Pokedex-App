91fd7f502173962fc46a8fc0c3e54cd76028603a
Artsiom Zhuk <artsiom.zhuk498@gmail.com>
2020-01-04 18:00:39 +0300

    change: change server port

--- src/server/server.js
+++ src/server/server.js
@@ -16,7 +16,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
 
-const port = 8282;
+const port = 8080;
 
 app.listen(port, () => {
   console.log(`Running Express on port - ${port}`);
