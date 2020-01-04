e0f3873f1d01d474463cd2b4bf46a18d8e562424
Artsiom Zhuk <artsiom.zhuk498@gmail.com>
2020-01-04 17:28:30 +0300

    fix: fix positioin routes

--- src/server/server.js
+++ src/server/server.js
@@ -4,11 +4,6 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname)));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
-});
-
-const port = 8282;
 
 app.get('*/info', (req, res) => {
   res.status(200).send('ok');
@@ -16,6 +11,12 @@ app.get('*/info', (req, res) => {
 app.get('*/health', (req, res) => {
   res.status(200).send('ok');
 });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+const port = 8282;
 
 app.listen(port, () => {
   console.log(`Running Express on port - ${port}`);
